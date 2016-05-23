f40fd95be567c9ad0ebab3585436c39d0f33520e
AlexZander <xander91@mail.ru>
2016-05-23 16:37:17 +0300

    новая структура с webpack

--- gulpfile.js
+++ gulpfile.js
@@ -1,4 +1,19 @@
 'use strict';
 
-var requireDir = require('require-dir');
-requireDir('./tasks', { recurse: true });
+const gulp = require('gulp');
+const plugins = require('gulp-load-plugins')();
+const config = require('./gulp/configs/main.config');
+const register = require(`./gulp/utils/register`);
+
+register(gulp, plugins, config)([
+    'build-vendor',
+    'build-js',
+    'build-scss',
+    'copy',
+    'serve',
+    'watch',
+    'deploy'
+]);
+
+gulp.task('build', gulp.parallel('build-vendor', 'build-js', 'build-scss', 'copy'));
+gulp.task('default', gulp.series('build', gulp.parallel('watch', 'serve')));
