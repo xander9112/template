237aad3a394885b9ae8296eec9c426630008c063
AlexZander <xander91@mail.ru>
2016-06-16 17:19:46 +0300

    Обновлены пакеты.

--- tasks/build.js
+++ tasks/build.js
@@ -1,12 +1,12 @@
 var gulp = require('gulp');
-var del = require('del');
+var rimraf = require('rimraf');
 
 var config = require('./config'),
 	production = config.production,
 	path = config.path;
 
 gulp.task('clean', function (callback) {
-	return del(path.clean, callback);
+	return rimraf(path.clean, callback);
 });
 
 gulp.task('build', [ 'scripts', 'styles', 'images', 'fonts', 'sprite' ]);
